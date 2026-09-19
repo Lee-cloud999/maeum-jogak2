ada12d5056bc11457dbc669fe184ef22526ab966
Lee-cloud999 <ggiyoo@gmail.com>
2026-09-19 15:19:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
-const C='mj2-v1';const A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const C='mj2-v2';
+const A=['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request)
+        .then(r=>{
+          const copy=r.clone();
+          caches.open(C).then(c=>c.put('./index.html',copy));
+          return r;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
